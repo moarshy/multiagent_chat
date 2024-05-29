63c2d4838e4ede3782ef37d79e1c73edd5abcde1
Richard Blythman <richardblythman@gmail.com>
2024-05-29 18:53:56 +0100

    new workflow definition

--- multiplayer_chat/run.py
+++ multiplayer_chat/run.py
@@ -1,118 +1,13 @@
 
-import asyncio
 from multiplayer_chat.schemas import InputSchema
-from multiplayer_chat.utils import get_logger
-# from naptha_sdk.client.node import Node
-import time
+from multiplayer_chat.utils import Workflow
 from typing import Dict
-import yaml
-
-logger = get_logger(__name__)
 
 async def run(inputs: InputSchema, nodes, workflow_config, cfg: Dict):
 
-    assert nodes is not None, "Multiplayer chat requires input list of node URLs."
+    workflow = Workflow("Multiplayer Chat", workflow_config)
 
-    logger.info(f"Coworker Nodes: {nodes[0].node_url}")
+    workflow.add_task(nodes[0], "chat", {"prompt": inputs.prompt})
+    workflow.add_task(nodes[1], "chat", {"prompt": inputs.prompt})
 
-    user_input = {
-        "public_key": workflow_config["consumer_id"].split(':')[1],
-        'id': workflow_config["consumer_id"],
-    }
-
-    logger.info(f"Checking user: {user_input}")
-    user = await nodes[0].check_user(user_input=user_input)
-
-    if user["is_registered"] == True:
-        print("Found user...", user)
-    elif user["is_registered"] == False:
-        print("No user found. Registering user...")
-        user = await nodes[0].register_user(user_input=user)
-        print(f"User registered: {user}.")
-
-    task_input = {
-        'consumer_id': workflow_config["consumer_id"],
-        "module_id": "chat",
-        "module_params": {"prompt": inputs.prompt},
-    }
-
-    logger.info(f"Running task: {task_input}")
-    job = await nodes[0].run_task(task_input=task_input, local=True)
-    logger.info(f"Job: {job}")
-    while True:
-        j = await nodes[0].check_task({"id": job['id']})
-
-        status = j['status']
-        logger.info(status)   
-
-        if status == 'completed':
-            break
-        if status == 'error':
-            break
-
-        time.sleep(3)
-
-    if j['status'] == 'completed':
-        logger.info(j['reply'])
-    else:
-        logger.info(j['error_message'])
-
-    print("Checking user...")
-    user = await nodes[1].check_user(user_input={
-        "public_key": workflow_config["consumer_id"].split(':')[1],
-        'id': workflow_config["consumer_id"],
-    })
-
-    if user["is_registered"] == True:
-        print("Found user...", user)
-    elif user["is_registered"] == False:
-        print("No user found. Registering user...")
-        user = await nodes[1].register_user(user_input=user)
-        print(f"User registered: {user}.")
-
-    job = await nodes[1].run_task(task_input={
-        'consumer_id': workflow_config["consumer_id"],
-        "module_id": "chat",
-        "module_params": {"prompt": j['reply']['output']}
-    }, local=True)
-
-    logger.info(f"Job ID: {job['id']}")
-    while True:
-        j = await nodes[1].check_task({"id": job['id']})
-
-        status = j['status']
-        logger.info(status)   
-
-        if status == 'completed':
-            break
-        if status == 'error':
-            break
-
-        time.sleep(3)
-
-    if j['status'] == 'completed':
-        logger.info(j['reply'])
-    else:
-        logger.info(j['error_message'])
-
-    return j['reply']
-
-if __name__ == "__main__":
-    cfg_path = "multiplayer_chat/component.yaml"
-    with open(cfg_path, "r") as file:
-        cfg = yaml.load(file, Loader=yaml.FullLoader)
-
-
-    args = {
-        "prompt": "hello, how are you doing?",
-        "coworkers": "http://node.naptha.ai:7001,http://node1.naptha.ai:7001"
-    }
-
-    job = {"consumer_id": "user:18837f9faec9a02744d308f935f1b05e8ff2fc355172e875c24366491625d932f36b34a4fa80bac58db635d5eddc87659c2b3fa700a1775eb4c43da6b0ec270d"}
-
-    inputs = InputSchema(**args)
-
-    # nodes = [Node(coworker) for coworker in args['coworkers'].split(',')]
-
-    response = asyncio.run(run(inputs, nodes, job, cfg))
-    logger.info(response)
+    return workflow
--- multiplayer_chat/utils.py
+++ multiplayer_chat/utils.py
@@ -1,14 +1,8 @@
-import logging
+class Workflow:
+    def __init__(self, name, workflow_config):
+        self.name = name
+        self.workflow_config = workflow_config
+        self.tasks = []
 
-
-def get_logger(name):
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    return logger
+    def add_task(self, node, task_name, *args):
+        self.tasks.append((node, task_name, args))
