ab5e83a77eb4eef26144ae4eed24df6841537c3c
Richard Blythman <richardblythman@gmail.com>
2024-05-30 20:29:21 +0100

    add tasks

--- multiplayer_chat/run.py
+++ multiplayer_chat/run.py
@@ -1,13 +1,33 @@
-
+import asyncio
 from multiplayer_chat.schemas import InputSchema
-from multiplayer_chat.utils import Workflow
+from naptha_sdk.task import Task
+from naptha_sdk.client.node import Node
 from typing import Dict
+import yaml
 
-async def run(inputs: InputSchema, nodes, workflow_config, cfg: Dict):
+async def run(inputs: InputSchema, worker_nodes, orchestrator_node, flow_run, cfg: Dict):
 
-    workflow = Workflow("Multiplayer Chat", workflow_config)
+    # workflow = Workflow("Multiplayer Chat", job)
 
-    workflow.add_task(nodes[0], "chat", {"prompt": inputs.prompt})
-    workflow.add_task(nodes[1], "chat", {"prompt": inputs.prompt})
+    task1 = Task(name="chat_initiator", fn="chat", worker_node=worker_nodes[0], orchestrator_node=orchestrator_node, task_run=flow_run)
+    task2 = Task(name="chat_receiver", fn="chat", worker_node=worker_nodes[1], orchestrator_node=orchestrator_node, task_run=flow_run)
 
-    return workflow
+    response1 = await task1(prompt=inputs.prompt)
+    response2 = await task2(prompt=response1)
+
+    return response2
+
+if __name__ == "__main__":
+    cfg_path = "multiplayer_chat/component.yaml"
+    with open(cfg_path, "r") as file:
+        cfg = yaml.load(file, Loader=yaml.FullLoader)
+    args = {
+        "prompt": "hello, how are you doing?",
+        "coworkers": "http://node.naptha.ai:7001,http://node1.naptha.ai:7001"
+    }
+    flow_run = {"consumer_id": "user:18837f9faec9a02744d308f935f1b05e8ff2fc355172e875c24366491625d932f36b34a4fa80bac58db635d5eddc87659c2b3fa700a1775eb4c43da6b0ec270d"}
+    inputs = InputSchema(**args)
+    orchestrator_node = Node("http://localhost:7001")
+    worker_nodes = [Node(coworker) for coworker in args['coworkers'].split(',')]
+    response = asyncio.run(run(inputs, worker_nodes, orchestrator_node, flow_run, cfg))
+    print(response)
